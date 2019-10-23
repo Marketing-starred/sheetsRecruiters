7ba68d3a6ed67d42cf1e0391a3f180e0cc45fb22
Romina Gomez <romina@starred.com>
2019-10-23 15:02:53 +0200

    Check sheets bar

--- public/js/main.js
+++ public/js/main.js
@@ -5,49 +5,12 @@ const $$ = document.querySelectorAll.bind(document);
 const docEventListener = document.addEventListener.bind(document);
 const on = addEventListener;
 
-// LazyLoad images 
-
-docEventListener("DOMContentLoaded", function() {
-  if (typeof LazyLoad === 'undefined' || LazyLoad === null) {
-    lazyLoadSafari(); 
-  } else {
-    var lazyLoadInstance = new LazyLoad({
-      elements_selector: ".lazy"
-      // ... more custom settings?
-    });
+document.addEventListener('scroll', function (event) {
+  if(window.pageYOffset>300){
+    $('.preview-sheets').classList.add("show-preview");
   }
-});
+}, false); 
 
-function lazyLoadSafari(){
-  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-  let active = false;
-  const lazyLoad = function() {
-  if (active === false) {
-    active = true;
-    setTimeout(function() {
-      lazyImages.forEach(function(lazyImage) {
-        if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
-          lazyImage.src = lazyImage.dataset.src;
-          lazyImage.srcset = lazyImage.dataset.srcset;
-          lazyImage.classList.remove("lazy");
-          lazyImages = lazyImages.filter(function(image) {
-            return image !== lazyImage;
-          });
-          if (lazyImages.length === 0) {
-            document.removeEventListener("scroll", lazyLoad);
-            window.removeEventListener("resize", lazyLoad);
-            window.removeEventListener("orientationchange", lazyLoad);
-          }
-        }
-      });
-      active = false;
-    }, 200);
-  }
-};
 
-docEventListener("scroll", lazyLoad);
-window.addEventListener("resize", lazyLoad);
-window.addEventListener("orientationchange", lazyLoad);
-}
-// *** End LazyLoad image
+
 
