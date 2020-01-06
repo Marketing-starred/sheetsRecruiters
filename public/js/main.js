ed5b4bbd4f084aa20b3d9e45b3fcff6796e33c2e
Romina Gomez <romina@starred.com>
2020-01-06 10:47:27 +0100

    Change wistia video

--- public/js/main.js
+++ public/js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
             <div></div>\
             <div></div>\
         </div>\
-        <script src="https://fast.wistia.com/embed/medias/ysh3zeeea2.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_ysh3zeeea2 videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/ysh3zeeea2/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>\
+        <div style="width:100%; height:0; position: relative; padding-bottom:56.25%"><iframe src="https://g16zktcb.videomarketingplatform.co/v.ihtml/player.html?token=b9bd120ab816fc6f1fcb08cf9b4d146a&source=embed&photo%5fid=58710372" style="width:100%; height:100%; position: absolute; top: 0; left: 0;" frameborder="0" border="0" scrolling="no" allowfullscreen="1" mozallowfullscreen="1" webkitallowfullscreen="1" allow="autoplay; fullscreen"></iframe></div>\
     </div>\
     <div class="color--white p-l-80 w-80">\
         <div class="footer-logo"><img src="https://cdn.sheetsforrecruiters.com/assets/starred-white-full.png" alt="Starred"></div>\
